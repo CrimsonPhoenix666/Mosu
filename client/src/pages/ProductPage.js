559ccbb6270785619b68039254fe97060ce06a74
CrimsonPhoenix666 <136012873+CrimsonPhoenix666@users.noreply.github.com>
2024-07-11 20:57:49 +0100

    Create ProductPage.js

---
+++ client/src/pages/ProductPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function ProductPage() {
+  return (
+    <div>
+      <h1>Product Details</h1>
+      {/* Add more content as needed */}
+    </div>
+  );
+}
+
+export default ProductPage;
