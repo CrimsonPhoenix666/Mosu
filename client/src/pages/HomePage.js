fc22db2290342bb328bb434ee11f8481a7d7079e
CrimsonPhoenix666 <136012873+CrimsonPhoenix666@users.noreply.github.com>
2024-07-11 20:57:33 +0100

    Create HomePage.js

---
+++ client/src/pages/HomePage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function HomePage() {
+  return (
+    <div>
+      <h1>Welcome to Mosu</h1>
+      {/* Add more content as needed */}
+    </div>
+  );
+}
+
+export default HomePage;
